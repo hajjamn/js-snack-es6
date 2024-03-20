8772f24a9694df4a2b34694a35adf74a38754dea
hajjamn <hajjamn@gmail.com>
2024-03-20 18:20:42 +0100

    ADD: snack 4 added array of strings for team names and functions to generate points and fouls

--- Snack 4/js/app.js
+++ Snack 4/js/app.js
@@ -5,3 +5,24 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
 
+//Creo la funzione per generare i numeri casuali
+  //Dichiaro quanti punti si ottengono vincendo una partita
+const winScore = 3;
+
+  //Scrivo una funzione che mi dia il punteggio che si ottiene vincendo le partite cogtro ogni avversario sia in andata che in ritorno
+function generateRandomScore() {
+  let max = (teams.number - 1) * winScore * 2;
+  let min = 0;
+  return parseInt(Math.random() * (max - min) + min)
+}
+
+  //Assumiamo che il numero di falli massimo ottenibile sia 15 a partita e generiamo il numero
+function generateRandomFouls() {
+  let max = (teams.number - 1) * 15;
+  let min = 0;
+  return parseInt(Math.random() * (max - min) + min)
+}
+
+//Genero un array di stringhe con i nomi delle squadre che voglio generare
+const teamsNames = ['Inter', 'Milan', 'Juventus', 'Bologna', 'Roma', 'Atalanta', 'Napoli', 'Fiorentina', 'Lazio', 'Monza', 'Torino', 'Genoa', 'Lecce', 'Udinese', 'Verona', 'Cagliari', 'Empoli', 'Frosinone', 'Sassuolo', 'Salernitana']
+console.log(teamsNames)
